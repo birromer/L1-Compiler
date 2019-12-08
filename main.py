19bc31c5dc77b13f3e36c140b1d2d77474890a5b
Bernardo Hummes <hummescmpa@gmail.com>
2019-12-08 15:31:58 -0300

    started main script for project

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  8 15:12:39 2019
+
+@author: birromer
+"""
+
+from install import install_packages
+from types import type_infer
+
+from lark import Lark
+from lark import exceptions
+from lark import Transformer
+import argpars
+
+
+
+
+
+
+
+if __name__ == "__main__":
+  install_packages()
+  
